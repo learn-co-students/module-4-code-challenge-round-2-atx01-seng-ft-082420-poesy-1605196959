b20e223fb30d3f2be8afd8290b25c82c37ffb9d8
alec-jasik <alec_jasik@yahoo.com>
2020-11-12 11:38:50 -0600

    submission for deadline

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React from "react";
 import "./App.css";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
+let poemsurl = "http://localhost:6001/poems"
 
 class App extends React.Component {
+  state = {
+    allPoems: [],
+    formToggle: "true"
+  }
+
+componentDidMount = () => {
+    fetch(poemsurl)
+    .then(res=>res.json())
+    .then(data=>this.setState({allPoems: data}))
+  }
+  // i know the toggle is wrong but i moved on to form bc i think i could finish it before time is up
+  handleToggle = () => {
+   const toggle = this.state.formToggle === "true" ? true : false
+   return toggle
+  }
+
+  addpoem = (poem) => {
+    this.setState({allPoems: [...this.state.allPoems, poem]})
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onclick = {()=>this.handleToggle()}>Show/hide new poem form</button>
+          {this.handleToggle() && <NewPoemForm />}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.allPoems} />
       </div>
     );
   }
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,13 +1,43 @@
 import React from "react";
+let poemsurl = "http://localhost:6001/poems"
 
 class NewPoemForm extends React.Component {
+  constructor(){
+    super()
+  this.state = this.getInitialState()
+  }
+
+  getInitialState = () => ({title: '', content:'', author:''})
+
+  handleChange(e, {title, value}) => this.setState({[title]: value})
+
+  handlesubmit = (e) => {
+    e.preventDefault()
+    const {title, content, author} = this.state
+    fetch(poemsurl, {
+      method: "POST",
+      headers: {
+      "content-type": "application/json",
+      "accept": "application/json"
+      },
+      body: JSON.stringify(
+        title,
+        content,
+        author
+      )
+      })
+      .then(resp => resp.json())
+      .then(poem => this.props.addpoem(poem))
+        
+  }
+
   render() {
     return (
       <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
+        <input placeholder="Title" onchange={handlechange}/>
+        <input placeholder="Author" onchange={handlechange} />
         <textarea placeholder="Write your masterpiece here..." rows={10} />
-        <input type="submit" value="Share your masterpiece" />
+        <input type="submit" value="Share your masterpiece" onchange={handlechange}/>
       </form>
     );
   }
