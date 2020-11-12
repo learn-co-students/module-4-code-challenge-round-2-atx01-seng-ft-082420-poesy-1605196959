90eedbfd0a6cd8ffcb14f835e60cc66190ed6902
christina888 <cechianelli@gmail.com>
2020-11-12 11:39:17 -0600

    submission

--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+
   render() {
     return (
       <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
-        <input type="submit" value="Share your masterpiece" />
+        <input onChange={this.props.handleChange} placeholder="Title" />
+        <input onChange={this.props.handleChange} placeholder="Author" />
+        <textarea onChange={this.props.handleChange} placeholder="Write your masterpiece here..." rows={10} />
+        <input onSubmit={(e)=>this.props.submitForm(e)} type="submit" value="Share your masterpiece" />
       </form>
     );
   }
--- src/Poem.js
+++ src/Poem.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 class Poem extends React.Component {
+
+  state = {
+    status: "Mark as Read"
+  }
+
+  markAsRead = () => {
+    console.log("test mark as read")
+    this.setState({
+      status: "Mark as Unread"
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Title</h3>
+        {this.props.title}
         <p>Content</p>
+        {this.props.content}
         <p>
           <strong>- By Author</strong>
+          {this.props.author}
         </p>
-        <button>Mark as read</button>
+        <button onClick={this.markAsRead}>{this.state.status}</button>
       </div>
     );
   }
